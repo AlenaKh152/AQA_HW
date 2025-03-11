54a810f60edf4e030c9ce094bb8f8bf474629c8f
Alena <elena.khulapava@gmail.com>
2025-03-12 00:29:47 +0300

    Fix comment

--- HW_13_cards_deck.py
+++ HW_13_cards_deck.py
@@ -4,25 +4,36 @@ import random
 class Card:
     number_list = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
     mast_list = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    card_list: list[str] = []
 
+    def __init__(self, number, mast):
+        self.number = number
+        self.mast = mast
+
+    def __str__(self):
+        return f'{self.number} {self.mast}'
+
+
+class CardsDeck:
     def __init__(self):
-        for i in self.number_list:
-            for j in self.mast_list:
-                self.card_list.append(i + ' ' + j)
-        self.card_list.append('Joker Red')
-        self.card_list.append('Joker Black')
+        self.cards_deck = []
+        self.create_deck()
 
+    def create_deck(self):
+        for i in Card.number_list:
+            for j in Card.mast_list:
+                self.cards_deck.append(Card(i, j))
+        self.cards_deck.append('Joker Red')
+        self.cards_deck.append('Joker Black')
 
-class CardsDeck(Card):
     def get(self, card_num):
-        return self.card_list[card_num - 1]
+        return self.cards_deck[card_num - 1]
 
     def shuffle(self):
-        return random.shuffle(self.card_list)
+        return random.shuffle(self.cards_deck)
 
 
 cd = CardsDeck()
+
 cd.shuffle()
 
 card_number = int(input('Choose the card from 54 cards deck '))
