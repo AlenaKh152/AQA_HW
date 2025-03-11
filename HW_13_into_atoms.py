736785001bf465c21feaf9b6f803aa81ac3acf60
Alena <elena.khulapava@gmail.com>
2025-03-11 17:44:42 +0300

    fix linters

--- HW_13_into_atoms.py
+++ HW_13_into_atoms.py
@@ -1,5 +1,4 @@
 import os
-
 os.system('cls')
 
 
@@ -17,61 +16,50 @@ class IntoAtoms:
 
         while i < len(self.molecula):
             if self.molecula[i].isalpha():
-                j = i + 1
-                if j < len(self.molecula) and self.molecula[j].islower():
-                    element = self.molecula[i:j + 1]
-                    i += 1
-                else:
-                    element = self.molecula[i]
-
-                count = 0
-
-                while j < len(self.molecula) and self.molecula[j].isdigit():
-                    if not self.molecula[j + 1].isdigit():
-                        count += int(self.molecula[j])
-                        j += 1
-                    else:  # если индекс после элемента двух, трёх и т.д -значное число
-                        k = 1
-                        el_count = self.molecula[j]
-                        while j + k < len(self.molecula) and self.molecula[j + k].isdigit():
-                            el_count += self.molecula[j + k]
-                            k += 1
-                        count = int(el_count)
-                        j += k
-
-                if not count:
-                    count = 1
-
-                atom_dict[element] = atom_dict.get(element, 0) + count
-
-
+                self.process_element(i, atom_dict)
             elif self.molecula[i] in open_braces:
                 dict_union_list.append(atom_dict)
                 atom_dict = {}
-
             elif self.molecula[i] in close_braces:
-                j = i + 1
-                count = 0
-                while j < len(self.molecula) and self.molecula[j].isdigit():
-                    count = count * 10 + int(self.molecula[j])
-                    j += 1
-
-                if not count:
-                    count = 1
-
-                for elem in atom_dict:
-                    atom_dict[elem] *= count
-
-                if dict_union_list:
-                    new_dict = dict_union_list.pop()
-                    for element, quantity in atom_dict.items():
-                        new_dict[element] = new_dict.get(element, 0) + quantity
-                    atom_dict = new_dict
+                atom_dict = self.process_close_brace(i, atom_dict, dict_union_list)
 
             i += 1
 
         self.result_dict = atom_dict
         return self.result_dict
+
+    def process_element(self, i, atom_dict):
+        j = i + 1
+        if j < len(self.molecula) and self.molecula[j].islower():
+            element = self.molecula[i:j + 1]
+            i += 1
+        else:
+            element = self.molecula[i]
+
+        count = self.get_element_count(j)
+        atom_dict[element] = atom_dict.get(element, 0) + count
+
+    def get_element_count(self, j):
+        count = 0
+        while j < len(self.molecula) and self.molecula[j].isdigit():
+            count = count * 10 + int(self.molecula[j])
+            j += 1
+
+        return 1 if not count else count
+
+    def process_close_brace(self, i, atom_dict, dict_union_list):
+        count = self.get_element_count(i + 1)
+
+        for elem in atom_dict:
+            atom_dict[elem] *= count
+
+        if dict_union_list:
+            new_dict = dict_union_list.pop()
+            for element, quantity in atom_dict.items():
+                new_dict[element] = new_dict.get(element, 0) + quantity
+            atom_dict = new_dict
+
+        return atom_dict
 
 
 molecula_1 = IntoAtoms('H2O')
