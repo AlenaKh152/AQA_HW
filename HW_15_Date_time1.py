e2ca201e2e7e425871d5189faa2356b251022a7e
Alena <elena.khulapava@gmail.com>
2025-03-18 17:20:40 +0300

    linters

--- HW_15_Date_time1.py
+++ HW_15_Date_time1.py
@@ -1,21 +1,4 @@
-'''from dateutil.parser import parse
-import os
-os.system('cls')
-
-date1 = parse(input('Enter the first date: '))
-date2 = parse(input('Enter the second date: '))
-
-
-def days_difference(date_1, date_2):
-    delta = abs((date_1 - date_2).days)
-    print(f'Number of days between dates: {delta}.')
-
-
-days_difference(date1, date2)'''
-
 from dateutil.parser import parse
-from dateutil.relativedelta import relativedelta
-import os
 import os
 os.system('cls')
 
@@ -24,15 +7,8 @@ date2 = parse(input('Enter the second date (YYYY-MM-DD): '))
 
 
 def days_difference(date_1, date_2):
-    # Вычисляем разницу между датами
-    delta = relativedelta(date_1, date_2)
-
-    # Вычисляем общее количество дней
-    total_days = abs((date_1 - date_2).days)
-
-    # Выводим результат
-    print(f'Number of days between dates: {total_days}.')
-    print(f'Difference is {delta.years} years, {delta.months} months, and {delta.days} days.')
+    delta = abs((date_1 - date_2).days)
+    print(f'Number of days between dates: {delta}.')
 
 
 days_difference(date1, date2)
