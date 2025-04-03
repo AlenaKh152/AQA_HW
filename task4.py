4eb62f13abf27da504ca736a051fc23e503feb1d
Alena <elena.khulapava@gmail.com>
2025-04-03 14:23:36 +0300

    fix lint

--- task4.py
+++ task4.py
@@ -1,6 +1,6 @@
 def add_one():
     num_list = list(input('Enter the number: '))
-    new_num_list =  list(str(int(''.join(num_list)) + 1))
+    new_num_list = list(str(int(''.join(num_list)) + 1))
     print(new_num_list)
 
 
