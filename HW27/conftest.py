63ce1450da4c020846384562deda2c562cd342f0
Alena <elena.khulapava@gmail.com>
2025-05-19 00:42:17 +0300

    hw27 refactor file path

--- HW27/conftest.py
+++ HW27/conftest.py
@@ -4,23 +4,23 @@ import pytest
 
 @pytest.fixture()
 def read_config():
-    with open("test_data/config.json") as f:
+    with open("HW27/test_data/config.json") as f:
         return json.load(f)
 
 
 @pytest.fixture()
 def read_create_temp():
-    with open("test_data/create_booking_temp.json") as f:
+    with open("HW27/test_data/create_booking_temp.json") as f:
         return json.load(f)
 
 
 @pytest.fixture()
 def read_user_creds():
-    with open("test_data/user_creds.json") as f:
+    with open("HW27/test_data/user_creds.json") as f:
         return json.load(f)
 
 
 @pytest.fixture()
 def read_partial_temp():
-    with open("test_data/partial_update_temp.json") as f:
+    with open("HW27/test_data/partial_update_temp.json") as f:
         return json.load(f)
